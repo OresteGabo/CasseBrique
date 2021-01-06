21a14d2a0492696c6294c8ab8660dfb8d7a624f3
Oreste <muhirwa.g.oreste@gmail.com>
2021-01-06 21:50:42 +0100

    ajout des tests

--- tests/CercleT.cpp
+++ tests/CercleT.cpp
@@ -1,14 +1,25 @@
-//#include"doctest.h"
+#include"doctest.h"
 #include"Cercle.h"
 
-/*
+
 TEST_CASE("Tests pour un cercle") {
     SUBCASE("La construction d'un cercle"){
         Cercle c{10.1,Position(10,10)};
-
-        REQUIRE_EQ(p.x(), x);
-        REQUIRE_EQ(p.y(), y);
+        REQUIRE_EQ(c.position(),Position(10,10));
+        REQUIRE_EQ(c.diametre(), 10.1*2);
+        REQUIRE_EQ(c.diametre(), 10.1*2);
+        REQUIRE_EQ(c.diametre(), 10.1*2);
     }
-
 }
-*/
+TESTCASE("Les tests des fonction qui nous renvois les valeurs"){
+    Cercle c(10.1,Position(10,10));
+    SUBCASE(" diametre"){
+        REQUIRE_EQ(c.diametre(),10.1*2);
+    }
+    SUBCASE(" rayon"){
+        REQUIRE_EQ(c.rayon(),10.1)
+    }
+    SUBCASE(" position"){
+        REQUIRE_EQ(c.position(),Position(10,10));
+    }
+}
